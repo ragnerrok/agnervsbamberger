cd776b2020744bbc4d75d86345669c6446e2e34c
Rachel Agner <rayagner@gmail.com>
2016-01-17 14:17:25 -0600

    adding first files

---
+++ js/setup.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Rachel on 1/16/2016.
+ */
+function init() {
+
+    //Side Menu
+    $( "#rsvp-button" ).button().click(function() {
+        console.log("Clicked RSVP");
+    });
+    $( "#location-button" ).button().click(function() {
+        console.log("Clicked location");
+    });
+    $( "#hotel-button" ).button().click(function() {
+        console.log("Clicked hotel");
+    });
+    $( "#registry-button" ).button().click(function() {
+        console.log("Clicked registry");
+    });
+    $( "#faq-button" ).button().click(function() {
+        console.log("Clicked faq");
+    });
+    $( "#contact-button" ).button().click(function() {
+        console.log("Clicked contact");
+    });
+
+
+    //RSVP Content
+    $( "#add-plus-one-button" ).button().click(function() {
+        console.log("Clicked contact");
+    });
+}
