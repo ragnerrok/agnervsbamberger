c53178f72cc3f056a6d98ec590461ff8dbac5c70
Aaron Bamberger <aaron.bamberger@gmail.com>
2016-01-18 00:00:25 -0600

    Added trimming and uppercasing to login code

--- php/login.php
+++ php/login.php
@@ -1,51 +1,13 @@
 <?php
 	include("create_db_conn.php");
-	
-	function get_music_suggestions($party_id, $db_conn) {
-		$music_query = $db_conn->prepare("CALL get_music_suggestions(:party_id)");
-		$music_query->bindParam(":party_id", $party_id);
-		$music_query->execute();
-		return $music_query->fetchAll(PDO::FETCH_ASSOC);
-	}
-	
-	function get_party_data($party_id, $db_conn) {
-		$party_data_query = $db_conn->prepare("CALL get_single_party(:party_id)");
-		$party_data_query->bindParam(":party_id", $party_id);
-		$party_data_query->execute();
-		$results = $party_data_query->fetchAll(PDO::FETCH_ASSOC);
-		return $results[0];
-	}
-	
-	function get_party_people($party_id, $db_conn) {
-		// First, get all of the people
-		$party_people_query = $db_conn->prepare("CALL get_people_in_party(:party_id)");
-		$party_people_query->bindParam(":party_id", $party_id);
-		$party_people_query->execute();
-		$people = $party_people_query->fetchAll(PDO::FETCH_ASSOC);
-		$party_people_query->closeCursor();
-		
-		// Next, get the allergy information for each person
-		$allergy_query = $db_conn->prepare("CALL get_allergies(:person_id)");
-		for ($i = 0; $i < count($people); ++$i) {
-			$allergy_query->bindParam(":person_id", $people[$i]["person_id"]);
-			$allergy_query->execute();
-			$allergies = array();
-			while ($allergy = $allergy_query->fetch(PDO::FETCH_ASSOC)) {
-				array_push($allergies, $allergy["food_allergy"]);
-			}
-			$people[$i]["allergies"] = $allergies;
-			$allergy_query->closeCursor();
-		}
-		
-		return $people;
-	}
+	include("db_utils.php");
 	
 	$login_successful = false;
 	$party_id = -1;
 	if (isset($_POST["login_code"])) {
 		$db_conn = open_db_conn();
 	
-		$login_code = $_POST["login_code"];
+		$login_code = strtoupper(trim($_POST["login_code"]));
 		$login_hash = md5($login_code);
 		
 		$login_query = $db_conn->prepare("CALL lookup_party_id(:login_hash)");
