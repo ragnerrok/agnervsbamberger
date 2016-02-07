9466b5bd1cd868826a4f85855c92bedbf53519ed
Aaron Bamberger <aaron.bamberger@gmail.com>
2016-02-06 18:17:12 -0600

    Added input validation to add or remove allergy

--- php/add_or_remove_allergy.php
+++ php/add_or_remove_allergy.php
@@ -1,7 +1,7 @@
 <?php
-	include("create_db_conn.php");
-	include_once("db_utils.php");
-	include("utils.php");
+	require("create_db_conn.php");
+	require_once("db_utils.php");
+	require_once("utils.php");
 	
 	function add_or_remove_allergy($add_or_remove) {
 		$return_value = array();
@@ -16,21 +16,27 @@
 			$allergy = $_POST["allergy"];
 			$db_conn = open_db_conn();
 			
-			if (authorize_request($party_id, $auth_token, $db_conn, $return_value)) {
-				$result = false;
-				if ($add_or_remove == "add") {
-					$result = add_allergy($person_id, $allergy, $db_conn);
-				} else {
-					$result = remove_allergy($person_id, $allergy, $db_conn);
-				}
-				
-				if (!$result) {
-					$return_value["status"] = false;
-					$return_value["reason"] = "Database Error";
-				} else {
-					$return_value["status"] = true;
-					$return_value["person_id"] = $person_id;
-					$return_value["allergy"] = $allergy;
+			// Validate client input
+			if (strlen($allergy) <= 0) {
+				$return_value["status"] = false;
+				$return_value["reason"] = "Must enter an allergy";
+			} else {
+				if (authorize_request($party_id, $auth_token, $db_conn, $return_value)) {
+					$result = false;
+					if ($add_or_remove == "add") {
+						$result = add_allergy($person_id, $allergy, $db_conn);
+					} else {
+						$result = remove_allergy($person_id, $allergy, $db_conn);
+					}
+					
+					if (!$result) {
+						$return_value["status"] = false;
+						$return_value["reason"] = "Database Error";
+					} else {
+						$return_value["status"] = true;
+						$return_value["person_id"] = $person_id;
+						$return_value["allergy"] = $allergy;
+					}
 				}
 			}
 		}
